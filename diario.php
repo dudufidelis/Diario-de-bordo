ead60b10f598c455675ea42565361d62fcc442db
Eduardo <duduecal12@gmail.com>
2023-08-05 16:15:32 -0300

    Alterações no css para ja ser utilizado

--- diario.php
+++ diario.php
@@ -18,27 +18,22 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 </head>
 <body>
     <div class="conteiner-diario">
-        <h1>Diário CPD</h1>
+        <h1>Diário CPD (Em desenvolvimento)</h1>
         <form action="adicionar_mensagem.php" method="post">
                 <textarea type="text" name="mensagem" placeholder="Deixe sua mensagem aqui!"></textarea>
                 <button type="submit">Enviar</button>
         </form>
 
-        <!-- Exibir a última mensagem -->
         <h2>Últimas Mensagens</h2>
         <?php include 'ultima_mensagem.php'; ?>
-        
-        <hr>
-        
-        <!-- Formulário para buscar mensagem por data -->
-        <h2>Buscar Mensagem por Data</h2>
-        <form action="buscar_mensagem.php" method="post">
-            <div class="mb-3">
-                <label for="data" class="form-label">Selecione a Data</label>
-                <input type="date" class="form-control" id="data" name="data">
-            </div>
-            <button type="submit" class="btn btn-primary">Buscar Mensagem</button>
-        </form>
+        <div class="search">
+            <h2>Buscar Mensagem por Data</h2>
+            <form action="buscar_mensagem.php" method="post">
+                    <label for="data">Selecione a Data</label>
+                    <input type="date" id="date-input" name="data">
+                <button type="submit">Buscar Mensagem</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
