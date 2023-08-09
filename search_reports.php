cd71a182c5f4876295d3474129fad4f18991a742
Eduardo <duduecal12@gmail.com>
2023-08-09 14:00:01 -0300

    comeco da estilizacao da pagina de filtro

--- search_reports.php
+++ search_reports.php
@@ -1,22 +1,44 @@
-<?php
-include 'connection.php';
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Diario | Mensagens Filtradas</title>
+    <link rel="stylesheet" href="style.css">
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $data_busca = $_POST["data"];
-    
-    $sql = "SELECT data, mensagem FROM mensagens WHERE data = '$data_busca'";
-    $result = $conn->query($sql);
+</head>
+<body>
+    <div class="container-filter">
+        <a class="back-link" href="diario.php">Voltar</a>
+        
+        <h2>Mensagens Filtradas por Data</h2>
+        
+        <?php
+        include 'connection.php';
 
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $data = $row["data"];
-            $mensagem = $row["mensagem"];
-            echo "<p>$data: $mensagem</p>";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $data_busca = $_POST["data"];
+            
+            $sql = "SELECT data, mensagem FROM mensagens WHERE data = '$data_busca'";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $data = $row["data"];
+                    $mensagem = $row["mensagem"];
+                    $dataFormatada = date("d/m/y", strtotime($data));
+
+                    echo "
+                    <div class='message'>
+                        <div class='date'>$dataFormatada</div>
+                        <p>$mensagem</p>
+                    </div>";
+                }
+            } else {
+                echo "<p>Nenhuma mensagem encontrada para essa data.</p>";
+            }
         }
-    } else {
-        echo "<p>Nenhuma mensagem encontrada para essa data.</p>";
-    }
-}
 
-include 'close_connection.php';
-?>
+        include 'close_connection.php';
+        ?>
+    </div>
+</body>
+</html>
