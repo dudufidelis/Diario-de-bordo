623af69c8199c9988100a0aa45fcbad36b19bc26
Eduardo <duduecal12@gmail.com>
2023-08-11 06:37:29 -0300

    criado pasta css

--- diario.php
+++ diario.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Diário de Bordo</title>
 </head>
 <body>
