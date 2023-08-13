3b43179e47cef1aa8ec248f3932438df61c389e7
Eduardo <duduecal12@gmail.com>
2023-08-13 15:25:34 -0300

    adicionado para entrar a data automatica

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+const date = new Date();
+const day = String(date.getDate()).padStart(2, '0');
+const month = String(date.getMonth() + 1).padStart(2, '0'); 
+const year = String(date.getFullYear()).slice(2);
+const formattedDate = `${day}/${month}/${year}`;
+
 document.getElementById("btn-batch").addEventListener("click", function(event) {
     event.preventDefault(); 
     infoBatch();
@@ -5,5 +11,5 @@ document.getElementById("btn-batch").addEventListener("click", function(event) {
 
 function infoBatch() {
     const inputTextArea = document.querySelector("#inputTextArea");
-    inputTextArea.value = 'Batch-OK - Data Batch 12/08/23-OK - PDV-OK - RUB-OK - Salvas-OK - sas77/sas08/sas24-OK - R.CARTÃO-OK - R.SACOLA-OK - ETIQUETA-OK';
+    inputTextArea.value = `Batch-OK - Data Batch ${formattedDate} - PDV-OK - RUB-OK - Salvas-OK - sas77/sas08/sas24-OK - R.CARTÃO-OK - R.SACOLA-OK - ETIQUETA-OK`;
 }
