b20ac80245b8341c23d9bd3c22a58691bc0fcbc5
Eduardo <duduecal12@gmail.com>
2023-08-19 13:41:14 -0300

    começo do design login

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login | CPD227</title>
+    <title>Team Sync | Login</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -20,16 +20,20 @@
     <div class="aviso">
         <h1>Projeto em<br>desenvolvimento</h1>
     </div>
-
-    <div class="conteiner">
-        <span class="material-symbols-outlined">
-            person
-        </span>
-        <form action="login.php" method="post">
-                <input type="text" name="username" placeholder="Usuário">        
-                <input type="password" name="password" placeholder="Senha">
+    <div class="login-conteiner">
+      <div class="login-img">
+        <img src="#" alt="#">
+      </div>
+      <div class="login-area">
+          <div class="logo">
+              <img src="assets/logo-transparency.png" alt="TeamSync Logo">
+          </div>
+          <form action="login.php" method="post">
+              <input type="text" name="username" placeholder="Usuário">        
+              <input type="password" name="password" placeholder="Senha">
                 <button type="submit">Entrar</button>
-        </form>
+          </form>
+      </div>
     </div>
   </body>
 </html>
